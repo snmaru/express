0eff9e800ad9c4293dcc0db254bfb9fba4d887c2
すぬま <snmaru@sunumanoMacBook-Air.local>
2025-01-26 19:37:22 +0900

    add env

--- routes/notes_from_b.js
+++ routes/notes_from_b.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
+
+
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://web7:D1mgAuqHT1TnwKNu@cluster0.he5n9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
-// idが１のドキュメントを取得
-const query = { id: 1 };
-const note = await notes.findOne(query);
+
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
 res.json(note);
 })
+
 module.exports = router;
